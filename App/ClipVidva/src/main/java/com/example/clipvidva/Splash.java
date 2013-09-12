50cc7a5983f7e9a50a7ddea4f5c43823c0f1688b
Nuttapon Pattanavijit <nuttt.p@gmail.com>
2013-09-12 23:43:55 +0700

    finish splash screen

--- App/ClipVidva/src/main/java/com/example/clipvidva/Splash.java
+++ App/ClipVidva/src/main/java/com/example/clipvidva/Splash.java
@@ -1,8 +1,12 @@
 package com.example.clipvidva;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.TextView;
 
 /**
  * Created by nuttt on 12/9/13.
@@ -12,5 +16,25 @@ public class Splash extends Activity {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.splash);
+
+        Typeface font = Typeface.createFromAsset(this.getAssets(), "fonts/fontawesome-webfont.ttf");
+        TextView icon_video = (TextView)findViewById(R.id.splash_watch_clip_icon);
+        icon_video.setTypeface(font);
+        TextView icon_pencil = (TextView)findViewById(R.id.splash_exam_icon);
+        icon_pencil.setTypeface(font);
+
+        Typeface font_text = Typeface.createFromAsset(this.getAssets(), "fonts/RSU_Regular.ttf");
+        TextView text_video = (TextView)findViewById(R.id.splash_watch_clip_text);
+        text_video.setTypeface(font_text);
+        TextView text_exam = (TextView)findViewById(R.id.splash_exam_text);
+        text_video.setTypeface(font_text);
+
+
+    }
+
+    public boolean goToCategory(View view){
+        Intent intent = new Intent(this, CategoryListActivity.class);
+        startActivity(intent);
+        return true;
     }
 }
